3e36da1d4690b3f009527e1e91d9ddd36e14af18
22A95A0513 <146426785+22A95A0513@users.noreply.github.com>
2023-11-10 15:57:39 +0530

    Delete Release2.java

--- Release2.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class Release2
-{
-	public static void main(String[] args)
-	{
-		System.out.println("---------------------------------------");
-		System.out.println("Hello Azure");
-	}
-}
